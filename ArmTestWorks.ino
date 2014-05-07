68761553c50f35c53baf0a6f7f560855acd89ae5
bhar92 <bhar92@gmail.com>
2014-05-08 02:19:23 +0530

    Update ArmTestWorks.ino

--- ArmTestWorks.ino
+++ ArmTestWorks.ino
@@ -1,92 +1,98 @@
-/* Sweep
- by BARRAGAN <http://barraganstudio.com> 
- This example code is in the public domain.
+#include <Servo.h>
+#include <Stepper.h>
 
- modified 8 Nov 2013
- by Scott Fitzgerald
- http://arduino.cc/en/Tutorial/Sweep
-*/ 
+const int stepsPerRevolution = 200;
 
-#include <Servo.h> 
- 
-Servo myservo10; // 180
-Servo myservo11; // 0
+Servo myservo;  // create servo object to control a servo
+Servo myservo10; 
+Servo myservo11; 
 Servo myservo9;
-                // twelve servo objects can be created on most boards
- 
-int pos = 0;    // variable to store the servo position 
- 
-void setup() 
-{ 
-  myservo10.attach(10);  // attaches the servo on pin 9 to the servo object 
-  myservo11.attach(11);
-  myservo9.attach(9);
-} 
- 
-void loop() 
-{ 
-  /*
-  myservo9.write(170);
-  
-  for(int i = 30; i < 91; i = i + 10){
-  myservo11.write(180-i);
-  myservo10.write(i);
-  delay(500);
-  
-  }
-  */
-  
- /* 
-  myservo10.write(50);
-  myservo11.write(180-50);
-  
-  for(int i = 70; i < 30; i++)
-  {
-    myservo9.write(i);
-    delay(50);
-  }
-*/
 
-//armUp();
-armDown();
-feetForward();
-flick();
-feetBackward();
-delay(3000);
+Stepper myStepper(stepsPerRevolution, 12, 8, 4, 2);
+
+int servoPin = 3;
+int enPin = 7;
+
+void setup(){
+    myservo.attach(servoPin);  // attaches the servo on pin 9 to the servo object
+    myservo10.attach(11);  // attaches the servo on pin 9 to the servo object 
+    myservo11.attach(10);
+    myservo9.attach(9);
+
+    myStepper.setSpeed(60);    // set speed to 30 rpm
+    pinMode(enPin, OUTPUT);
+    digitalWrite(enPin, LOW);
+    Serial.begin(9600);  
 }
+
+void loop() { 
+    char c = Serial.read();
+    
+    if(c == 'd'){
+        myservo.write(180);
+        delay(15);  
+    }
+    
+    if(c == 'u'){
+        myservo.write(90);  
+        delay(15);  
+    }
+        
+    if(c == 'l'){
+        digitalWrite(enPin, HIGH);
+        myStepper.step(55);
+    }
+    
+    if(c == 'r'){
+        digitalWrite(enPin, HIGH);
+        myStepper.step(-55);
+    }
+    
+    if(c == 'f'){
+        armDown();
+        feetForward();
+        flick();
+        feetBackward();
+    }
+    
+  
+    digitalWrite(enPin, LOW);
+} 
 
 void feetForward(void){
   myservo10.write(60);
   myservo11.write(180-70);
-  delay(500);
+  delay(250);
 }  
 
 void feetBackward(void){
   myservo10.write(90);
   myservo11.write(180-90);
-  delay(500);
+  delay(250);
 }  
 
 
 void armUp(void){
   myservo9.write(80);
-  delay(500);
+  delay(250);
 }  
 
 void armDown(void){
   myservo9.write(120);
-  delay(500);
+  delay(250);
 }
 
 
 void flick(void){
-   for(int i = 120; i > 81; i--)
+  for(int i = 120; i > 81; i--)
   {
     myservo9.write(i);
-    delay(15);
+    delay(10);
   }
+  
   delay(1000);
 }
+
 
 
 
